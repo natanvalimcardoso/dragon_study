25bde1950e60d8cd6b50477f9b9db591f6482377
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-12-27 08:26:44 -0300

    ready

--- lib/getx_rodrigo/pages/pokemons_page.dart
+++ lib/getx_rodrigo/pages/pokemons_page.dart
@@ -4,42 +4,44 @@ import 'package:push_notification_firebase/getx_rodrigo/controller/pokemon_contr
 
 import '../../shared/models/pokemon_model.dart';
 
-
 class PokemonsPage extends GetView<PokemonController> {
   const PokemonsPage({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Dio Page Rodrigueira'),
       ),
-      body: controller.obx((state) {
-        final List<Pokemon> pokemons = state;
-        return ListView.builder(
-          itemCount: pokemons.length,
-          itemBuilder: (context, index) {
-            final pokemon = pokemons[index].name;
-            return ListTile(
-              title: Text(pokemon.toString()),
-            );
-          },
-        );
-      }, onError: (error) {
-        return SizedBox(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(error!),
-              TextButton(
-                onPressed: () => controller.getPokemon(),
-                child: Text('Tentar novamente'),
-              )
-            ],
-          ),
-        );
-      }),
+      body: controller.obx(
+        (state) {
+          final List<Pokemon> pokemons = state;
+          return ListView.builder(
+            itemCount: pokemons.length,
+            itemBuilder: (context, index) {
+              final pokemon = pokemons[index].name;
+              return ListTile(
+                title: Text(pokemon.toString()),
+              );
+            },
+          );
+        },
+        onError: (error) {
+          return SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(error!),
+                TextButton(
+                  onPressed: () => controller.getPokemon(),
+                  child: Text('Tentar novamente'),
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
