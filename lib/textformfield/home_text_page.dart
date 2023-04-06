c11ada2e38bc145cb058a06bf8a249e6618bbf2a
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2023-04-06 13:53:07 -0300

    ready

--- lib/textformfield/home_text_page.dart
+++ lib/textformfield/home_text_page.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
 
-class HomeTextPage extends StatelessWidget {
+import 'input_text_field_widget.dart';
 
-  const HomeTextPage({ Key? key }) : super(key: key);
+class HomeTextPage extends StatefulWidget {
+  const HomeTextPage({Key? key}) : super(key: key);
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Texte Textfield'),),
-           body: Container(),
-       );
+  @override
+  State<HomeTextPage> createState() => _HomeTextPageState();
+}
+
+class _HomeTextPageState extends State<HomeTextPage> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha página'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                EmailFormField(
+                  labelText: 'E-mail',
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Enviando...'),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('Enviar'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/textformfield/input_text_field_widget.dart
+++ lib/textformfield/input_text_field_widget.dart
@@ -1,15 +1,60 @@
-
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter/material.dart';
 
-class InputTextFieldWidget extends StatelessWidget {
+class EmailFormField extends StatefulWidget {
+  final String labelText;
 
-  const InputTextFieldWidget({ Key? key }) : super(key: key);
+  EmailFormField({required this.labelText});
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           body: Container(),
-       );
+  @override
+  _EmailFormFieldState createState() => _EmailFormFieldState();
+}
+
+class _EmailFormFieldState extends State<EmailFormField> {
+  final TextEditingController _emailController = TextEditingController();
+  bool _isValid = true;
+  String _errorMessage = '';
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  bool _validateEmail(String email) {
+    final emailRegex = RegExp(r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$');
+    return emailRegex.hasMatch(email);
+  }
+
+  void _onEmailChanged(String value) {
+    setState(() {
+      if (value.isEmpty) {
+        _isValid = false;
+        _errorMessage = 'Campo obrigatório';
+      } else if (!_validateEmail(value)) {
+        _isValid = false;
+        _errorMessage = 'E-mail inválido';
+      } else {
+        _isValid = true;
+        _errorMessage = '';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: _emailController,
+      decoration: InputDecoration(
+        labelText: widget.labelText,
+        border: OutlineInputBorder(),
+      ),
+      onChanged: _onEmailChanged,
+      validator: (value) {
+        if (!_isValid) {
+          return _errorMessage;
+        }
+        return null;
+      },
+    );
   }
 }
